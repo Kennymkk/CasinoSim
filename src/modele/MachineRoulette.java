d15dd1eedf590045ddc1f09f112c5d6104299402
Kennymkk <37218561+Kennymkk@users.noreply.github.com>
2018-12-12 18:51:47 +0100

    files via upload thanks 3il
    aa

---
+++ src/modele/MachineRoulette.java
@@ -0,0 +1,15 @@
+package modele;
+
+public class MachineRoulette extends Ab_Machine{
+
+	private I_RegleGain rules;
+
+	@Override
+	public int jouer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+}
